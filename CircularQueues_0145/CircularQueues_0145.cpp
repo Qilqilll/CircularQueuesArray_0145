9d81515b7ef323e3e00078eb4b9d1ff694f2c044
Qilqilll <aqilf612@gmail.com>
2024-05-31 11:33:10 +0700

    menampilkan display

--- CircularQueues_0145/CircularQueues_0145.cpp
+++ CircularQueues_0145/CircularQueues_0145.cpp
@@ -92,3 +92,14 @@ public:
 int main() {
 	Queues q;
 	char ch;
+
+	while (true) {
+		try {
+			cout << "Menu" << endl;
+			cout << "1. Implement insert operation" << endl;
+			cout << "2. Implement delete operation" << endl;
+			cout << "3. Display values" << endl;
+			cout << "4. Exit" << endl;
+			cout << "Eter your choise (1-4): ";
+			cin >> ch;
+			cout << endl;
