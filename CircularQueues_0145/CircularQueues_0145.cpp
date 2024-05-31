9da922f63b4260c739227672e786f61f033ce333
Qilqilll <aqilf612@gmail.com>
2024-05-31 11:33:54 +0700

    menjalankan program ke display yg ditampilkan

--- CircularQueues_0145/CircularQueues_0145.cpp
+++ CircularQueues_0145/CircularQueues_0145.cpp
@@ -103,3 +103,25 @@ int main() {
 			cout << "Eter your choise (1-4): ";
 			cin >> ch;
 			cout << endl;
+
+			switch (ch) {
+			case '1': {
+				q.insert();
+				break;
+			}
+			case '2': {
+				q.remove();
+				break;
+			}
+			case '3': {
+				q.display();
+				break;
+			}
+			case '4': {
+				return 0;
+			}
+			default: {
+				cout << "Invalid option!!" << endl;
+				break;
+			}
+			}
